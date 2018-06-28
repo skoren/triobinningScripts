47dc21b331722f62a33c8514a0548685d7521872
Arang Rhie <arangrhie@users.noreply.github.com>
2018-06-27 21:00:01 -0400

    Create _meryl_mrg.sh

---
+++ update/_meryl_mrg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+k=$1
+prefix=$2
+filelist=$3
+
+for file in $(grep $prefix $filelist);
+do
+name=${file/.fastq.gz/}
+name=${name/.fq.gz/}
+indb=$indb" -s $name.$k"
+done
+
+mrg=$prefix.k$k
+
+echo "\
+meryl -M add $indb -o $mrg"
+meryl -M add $indb -o $mrg
+
+echo "\
+meryl -Dh -s $mrg > $mrg.hist"
+meryl -Dh -s $mrg > $mrg.hist
+
+
+
+echo "java -jar -Xmx512m kmerHistToPloidyDepth.jar $mrg.hist > $mrg.hist.ploidy"
+java -jar -Xmx512m kmerHistToPloidyDepth.jar $mrg.hist > $mrg.hist.ploidy
+
+x=`sed -n 2p $mrg.hist.ploidy | awk '{print $NF}'`
+
+echo "\
+meryl -M greaterthan $x -s $mrg -o $mrg.filt"
+meryl -M greaterthan $x -s $mrg -o $mrg.filt
+
+echo "\
+meryl -Dc -s $mrg.filt"
+meryl -Dc -s $mrg.filt
+
+echo ""
+
+echo "Ready to subtract"
